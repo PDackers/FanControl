91a21761ad544445b80ce60bb255824690614b40
PDackers <104564615+PDackers@users.noreply.github.com>
2022-05-11 15:38:47 +0000

    Update fancontrol.py
    New version which works with Python 3.7 and can be used with CRON.

--- fancontrol.py
+++ fancontrol.py
@@ -1,51 +1,47 @@
 #!/usr/bin/env python3
 
 import re
+import RPi.GPIO as GPIO
 import subprocess
 import syslog
+import time
 
-from gpiozero import OutputDevice
-
-ON_THRESHOLD = 50  # (degrees Celsius) Fan starts at this temperature.
-OFF_THRESHOLD = 40  # (degress Celsius) Fan stops at this temperature.
-GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
+GPIO.setmode(GPIO.BCM)  # Make use of the BCM-notation.
+GPIO.setup(17, GPIO.OUT, initial=0)  # Initially, set BCM17 to output and off.
+ON_THRESHOLD = 50.0  # (degrees Celsius) Fan starts at or above this temperature.
 
 
-def get_temp():
+def get_temperature():
     """
     Get the core temperature.
-    Run a shell script to get the core temp and parse the output.
-    Raises a 'RuntimeError' if response cannot be parsed.
+    Run a subprocess to get the core temperature and parse the output.
+    Raises a 'RuntimeError' if the response cannot be parsed.
     Returns a 'float' with the core temperature in degrees Celsius.
     """
     command = subprocess.run(["vcgencmd", "measure_temp"], capture_output=True)
     output = command.stdout.decode()
     match = re.search(r"\d+\.\d", output)
-
     try:
         return float(match.group())
     except (IndexError, TypeError, ValueError):
         raise RuntimeError("Could not parse temperature output.")
 
 
-if __name__ == "__main__":
-    # Validate the 'on' and 'off' thresholds.
-    if OFF_THRESHOLD >= ON_THRESHOLD:
-        raise RuntimeError("OFF_THRESHOLD must be less than ON_THRESHOLD.")
-
-    fan = OutputDevice(GPIO_PIN)
-    temp = get_temp()
-
+def main():
+    """
+    Get the core temperature and check if it's higher than the value
+    specified in 'ON_THRESHOLD'. If the temperature is higher, turn on
+    BCM17 for 120 seconds to cool down the Raspberry Pi 4 (Pi-hole).
+    """
+    temperature = get_temperature()
     try:
-        # Start the fan if the temperature has reached the limit and the fan
-        # isn't already running. Note: 'fan.value' returns either 0 or 1.
-        if temp > ON_THRESHOLD and not fan.value:
-            fan.on()
-            syslog.syslog("Fan started")
-        # Stop the fan if the fan is running and the temperature has dropped
-        # to or below the 'off' threshold.
-        elif fan.value and temp < OFF_THRESHOLD:
-            fan.off()
-            syslog.syslog("Fan stopped")
+        if temperature > ON_THRESHOLD:
+            GPIO.output(17, 1)  # Activate BCM17 and start the fan.
+            time.sleep(120)
     except (IndexError, TypeError, ValueError):
-        raise RuntimeError("Could not start or stop the fan.")
+        raise RuntimeError("Could not check the temperature.")
+
+
+if __name__ == "__main__":
+    main()
+    GPIO.cleanup()  # BCM17 will be reset to its default setting.
